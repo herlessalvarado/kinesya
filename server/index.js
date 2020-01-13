60e1be546cdd6684e5d24211a25ae9298b44818c
Herless Alvarado <herlessalvarado@Herlesss-MacBook-Pro.local>
2020-01-13 18:58:36 -0500

    mongodb cloud connected

--- server/index.js
+++ server/index.js
@@ -1,6 +1,8 @@
 const express = require('express');
+const connectDB = require('./Repository/Connection');
 const app = express();
 
+connectDB();
 const Port = process.env.Port || 8000;
 
 app.listen(Port, () => console.log('Server Started'));
