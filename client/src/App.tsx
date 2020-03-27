e80b6d3de8c7af21ca542f033466a00f3a254a98
Herless Alvarado <herlessalvarado@Herlesss-MacBook-Pro.local>
2020-03-27 10:09:47 -0500

    card created

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,5 +1,10 @@
 import React from 'react';
 import Header from './components/Header';
+import { CssBaseline } from '@material-ui/core';
+import Container from '@material-ui/core/Container';
+import ProfileCard from './components/Card';
+import Whoreimg from './assets/62.jpg';
+//import Grid from '@material-ui/core/Grid';
 
 let sections: {title: string, url: string} [] = [
   {title : "Login" , url : "#"},
@@ -7,9 +12,22 @@ let sections: {title: string, url: string} [] = [
   {title : "About", url : "#"}
 ]
 
+let profile: {name: string, image: string, description: string, price: Number} [] = [
+  {name: "Putita", image: Whoreimg, description: "La mas zorra de todo puente nuevo", price: 250}
+]
+
 function App() {
   return (
-    <Header title="PK" sections={sections}></Header>
+    <React.Fragment>
+      <CssBaseline>
+      <Container maxWidth="lg">
+        <Header title="PK" sections={sections}></Header>
+        <main>
+          <ProfileCard name="Putita" image={Whoreimg} description="La mas zorra de todo puente nuevo" price={250}></ProfileCard>
+        </main>
+    </Container>
+    </CssBaseline>
+    </React.Fragment>
   );
 }
 
