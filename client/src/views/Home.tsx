e1095f9f33d88028fdd663fe1c8d0eed0b4b8294
Jose <jose45321@outlook.com>
2020-04-06 19:25:35 -0500

    Terminado

--- client/src/views/Home.tsx
+++ client/src/views/Home.tsx
@@ -1,22 +1,36 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import Header from '../views/Header';
 import { CssBaseline } from '@material-ui/core';
 import Container from '@material-ui/core/Container';
 import Main from '../views/Main';
+import { checkAuth } from '../cache/CookieManager';
 
 let sections: {title: string, url: string} [] = [
-  {title : "Login" , url : "/login"},
   {title : "Clients", url : "/clients"},
-  {title : "About", url : "/about"}
+  {title : "About", url : "/about"},
+  {title : "Login" , url : "/login"}
+]
+
+let sections2: {title: string, url: string} [] = [
+  {title : "Clients", url : "/clients"},
+  {title : "About", url : "/about"},
+  {title : "Logout" , url : "/login"}
 ]
 
 export default function Home() {
-  
+  const [auth, setAuth] = useState(false);
+
+
+  useEffect (() => {
+    if(!!checkAuth()){
+      setAuth(true);
+    }
+  }, [])
   return (
     <React.Fragment>
       <CssBaseline>
       <Container maxWidth="lg">
-        <Header title="PK" sections={sections}></Header>
+        {auth ? <Header title="PK" sections={sections2}></Header> : <Header title="PK" sections={sections}></Header> }
         <main>
           <Main></Main>
         </main>
