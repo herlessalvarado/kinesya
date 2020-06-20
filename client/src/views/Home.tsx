f2ee22724e898cf706fc27be1d353a5d4e8142d6
Herless Alvarado <38076809+herlessalvarado@users.noreply.github.com>
2020-06-20 15:36:29 -0500

    testing CD

--- client/src/views/Home.tsx
+++ client/src/views/Home.tsx
@@ -142,7 +142,7 @@ export default function Home() {
                         getOptionLabel={(option) => option}
                         onChange={handleSelected}
                         renderInput={(params) => {
-                            return <TextField {...params} placeholder="Buscando..." />
+                            return <TextField {...params} placeholder="Search..." />
                         }}
                     />
                 </div>
