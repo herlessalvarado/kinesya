efc686b9d52b380c4dcdcc6986d2e5d34332a34f
Herless Alvarado <herlessalvarado@gmail.com>
2020-07-06 17:24:13 -0500

    translations

--- client/src/components/policy/PrivacyPolicy.tsx
+++ client/src/components/policy/PrivacyPolicy.tsx
@@ -1,9 +1,12 @@
 import React from "react";
+import { useTranslation } from 'react-i18next';
 
 export default  function (){
+	const { t } = useTranslation('common')
+
     return (
 		<React.Fragment>
-			<h1>Política de privacidad</h1>
+			<h1>{t("terms.policy.title")}</h1>
 		</React.Fragment>
 	);
 }
